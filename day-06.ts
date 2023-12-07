0963ac651ad75176392cfce8cf2df31824192063
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2023-12-07 16:36:20 +0100

    day six, part two

--- day-06.ts
+++ day-06.ts
@@ -1,72 +1,43 @@
 import { assert } from "./utils";
 
-type Race = {
+type World = {
   time: number;
   /** record distance */
   distance: number;
 };
 
-type World = Race[];
-
-const isFastEnough = (race: Race, holdTime: number): boolean => {
+const isFastEnough = (race: World, holdTime: number): boolean => {
   return race.time * holdTime - holdTime * holdTime > race.distance;
 };
 
-const calcPossibleTimes = (race: Race): number[] => {
+const calcPossibleTimes = (race: World): number => {
   let found: number[] = [];
   for (let i = 0; i < race.time; i++) {
     if (isFastEnough(race, i)) {
       found.push(i);
     }
   }
-  return found;
+  return found.length;
 };
 
-const calcResult = (world: World): number =>
-  world
-    .map(calcPossibleTimes)
-    .map((x) => x.length)
-    .reduce((curr, acc) => curr * acc, 1);
-
 const runTests = () => {
-  const test: World = [
-    {
-      time: 7,
-      distance: 9,
-    },
-    {
-      time: 15,
-      distance: 40,
-    },
-    {
-      time: 30,
-      distance: 200,
-    },
-  ];
-  assert(calcResult(test), 288);
+  const test: World = {
+    time: 71530,
+    distance: 940200,
+  };
+  assert(calcPossibleTimes(test), 71503);
 };
 
 // Time:        62     64     91     90
 // Distance:   553   1010   1473   1074
 export const main = () => {
-  const data: World = [
-    { time: 62, distance: 553 },
-    {
-      time: 64,
-      distance: 1010,
-    },
-    {
-      time: 91,
-      distance: 1473,
-    },
-    {
-      time: 90,
-      distance: 1074,
-    },
-  ];
+  const world: World = {
+    time: 62649190,
+    distance: 553101014731074,
+  };
   runTests();
 
-  const result = calcResult(data);
+  const result = calcPossibleTimes(world);
   console.log("The result is", result);
 };
 
